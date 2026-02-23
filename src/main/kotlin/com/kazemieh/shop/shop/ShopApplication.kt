bee0336fea55dacc5c5878bce0f981503232c597
puria kazemieh <puriakazemieh@gmail.com>
2026-02-23 11:21:31 +0330

    initial commit

---
+++ src/main/kotlin/com/kazemieh/shop/shop/ShopApplication.kt
@@ -0,0 +1,11 @@
+package com.kazemieh.shop.shop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShopApplication>(*args)
+}
