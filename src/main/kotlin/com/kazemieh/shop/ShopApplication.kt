2bf6feee3e7bac57b9e32e2ba850c0ab12e77ab0
puriakazemieh <puriakazemieh@gmail.com>
2026-05-25 12:05:04 +0330

    [FEATURE] handle run app

--- src/main/kotlin/com/kazemieh/shop/ShopApplication.kt
+++ src/main/kotlin/com/kazemieh/shop/ShopApplication.kt
@@ -1,12 +1,22 @@
 package com.kazemieh.shop
 
+import com.kazemieh.shop.shared.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-@EnableScheduling // فعال‌سازی قابلیت زمان‌بندی
-class ShopApplication
+@EnableScheduling
+@EnableConfigurationProperties(JwtProperties::class)
+class ShopApplication {
+    @Bean
+    fun restTemplate(): RestTemplate {
+        return RestTemplate()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ShopApplication>(*args)
