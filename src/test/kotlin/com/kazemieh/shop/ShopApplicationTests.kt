75bb714e7fb6f8345d53c6dc1cf9f28172ef2203
puriakazemieh <puriakazemieh@gmail.com>
2026-08-26 10:50:44 +0330

    feat(qa): P02-QA-CODE-008 Setup Testcontainers for independent testing

--- src/test/kotlin/com/kazemieh/shop/ShopApplicationTests.kt
+++ src/test/kotlin/com/kazemieh/shop/ShopApplicationTests.kt
@@ -2,9 +2,22 @@ package com.kazemieh.shop
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class ShopApplicationTests {
+
+    companion object {
+        @Container
+        @ServiceConnection
+        val postgresContainer = PostgreSQLContainer("postgres:15-alpine")
+    }
 
     @Test
     fun contextLoads() {
